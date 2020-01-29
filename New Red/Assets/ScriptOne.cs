c94b04c9b35d3407d8606e48c5d5c50316fb65c4
ahumpherys <aaronhumpherys@icloud.com>
2020-01-29 11:12:49 -0700

    1/29/2020
    OnMouseEvents

--- New Red/Assets/ScriptOne.cs
+++ New Red/Assets/ScriptOne.cs
@@ -1,8 +1,35 @@
-﻿using System.Collections;
+﻿using System;
+using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.Events;
 
 public class ScriptOne : MonoBehaviour
 {
-    //Test
+    public UnityEvent MouseDownEvent, OnMouseUpEvent, OnMouseEnterEvent, OnMouseDragEvent, OnMouseExitEvent;
+
+    public void OnMouseExit()
+    {
+        OnMouseExitEvent.Invoke();
+    }
+
+    public void OnMouseUp()
+    {
+        OnMouseUpEvent.Invoke();
+    }
+
+    public void OnMouseEnter()
+    {
+        OnMouseEnterEvent.Invoke();
+    }
+
+    public void OnMouseDrag()
+    {
+        OnMouseDragEvent.Invoke();
+    }
+
+    private void OnMouseDown()
+    {
+        MouseDownEvent.Invoke();
+    }
 }
