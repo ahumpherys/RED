e9845eabf5e1caeeb61ca3c4528a8ee0a80d52d0
Aaron <10828507@my.uvu.edu>
2020-02-03 11:21:18 -0700

    2/3/2020

---
+++ New Red/Assets/CharacterMovement.cs
@@ -0,0 +1,30 @@
+﻿using UnityEngine;
+
+public class CharacterMovement : MonoBehaviour
+{
+    public CharacterController controller;
+    public Vector3 positionDirection;
+    public float speed = 5f;
+    public float gravity = -3f;
+    public float jumpForce = 50f;
+
+    void Start()
+    {
+        
+    }
+
+    void Update()
+    {
+        positionDirection.x = Input.GetAxis("Horizontal") * speed;
+        if (Input.GetButtonDown("Jump"))
+        {
+            positionDirection.y = jumpForce;
+        }
+        positionDirection.y += gravity;
+        controller.Move(positionDirection * Time.deltaTime);
+
+       // positionDirection.z = Input.GetAxis("Horizontal") * speed;
+
+    }
+    
+}
