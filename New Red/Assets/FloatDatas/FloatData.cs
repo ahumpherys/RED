56afe63464c823c336743500f774b08e98802f85
Aaron <10828507@my.uvu.edu>
2020-02-19 11:18:12 -0700

    2/19/2020
    first part of health bar

---
+++ New Red/Assets/FloatDatas/FloatData.cs
@@ -0,0 +1,13 @@
+﻿using System.Globalization;
+using UnityEngine;
+
+[CreateAssetMenu]
+public class FloatData : ScriptableObject
+{
+    public float value = 1f;
+
+  //  public void ChangeValue(float number);
+  //  {
+   //     value += number;
+    //}
+}
---
+++ New Red/Assets/Int Datas/Int.cs
@@ -0,0 +1,7 @@
+﻿using UnityEngine;
+
+[CreateAssetMenu]
+public class Int : ScriptableObject
+{
+   public int value = 1;
+}
