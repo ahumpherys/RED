f7a6642ed48e88fa330d0622e5c1bb53fc7fc9b9
Aaron <10828507@my.uvu.edu>
2020-03-04 11:24:13 -0700

    3/4/2020
    Checkpoints

---
+++ New Red/Assets/Checkpoint,StringData,VectorData/StringData.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+[CreateAssetMenu]
+public class StringData : ScriptableObject
+{
+ public string value;
+
+ public void ChangeValue(string number)
+ {
+  value += number;
+ }
+}
---
+++ New Red/Assets/Checkpoint,StringData,VectorData/Vector3Data.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+[CreateAssetMenu]
+public class Vector3Data : ScriptableObject
+{
+    public Vector3 value;
+
+    public void ChangePosition(Transform transformOBJ)
+    {
+        transformOBJ.position = value;
+    }
+
+    public void ChangeValueFromTransform(Transform transformOBJ)
+    {
+        value = transformOBJ.position;
+    }
+    
+    public void ChangeVector3Data(Vector3Data vector3DataOBJ)
+    {
+        vector3DataOBJ.value = value;
+    }
+    
+    
+}
