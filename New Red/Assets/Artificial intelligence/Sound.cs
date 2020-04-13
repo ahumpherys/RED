398ed3fc86c1708b0ca029d9586384dc77b528db
ahumpherys <aaronhumpherys@icloud.com>
2020-04-13 11:03:30 -0600

    4/16/2020

---
+++ New Red/Assets/Artificial intelligence/Sound.cs
@@ -0,0 +1,22 @@
+﻿using UnityEngine;
+
+public class Sound : MonoBehaviour
+{
+    public AudioSource sound;
+    public GameObject ammo;
+    
+    void Update()
+    {
+        if (Input.GetButtonDown("fire1"))
+        {
+            Shot();
+        }
+    }
+
+    void Shot()
+    {
+        Instantiate(ammo, transform.position, transform.rotation);
+        sound.Play();
+    }
+}
+//code from following along in class
