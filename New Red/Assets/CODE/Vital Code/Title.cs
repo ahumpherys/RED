648d5b4b4e6cac2e924fed3737e1756aa29ac7b2
ahumpherys <aaronhumpherys@icloud.com>
2020-04-27 23:10:48 -0600

    Final Game

---
+++ New Red/Assets/CODE/Vital Code/Title.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class Title : MonoBehaviour
+{
+    public string LevelOne;
+
+    public void NewGame()
+    {
+        SceneManager.LoadScene("LavaGame");
+    }
+}
